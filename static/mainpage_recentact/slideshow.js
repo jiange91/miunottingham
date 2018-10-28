399f5c3d7e17537ae70bbf40ee2c647549b22f84
zijian <eric@example.com>
2018-10-29 02:15:57 +0800

    nothing just reading

--- static/mainpage_recentact/slideshow.js
+++ static/mainpage_recentact/slideshow.js
@@ -3,14 +3,11 @@ var intervalId = 0;
 
 const prevBtn = document.querySelector('.prev')
 prevBtn.addEventListener('click', () => updateSlideIndex(slideIndex-1))
-prevBtn.addEventListener('touchstart', () => updateSlideIndex(slideIndex-1))
 const nextBtn = document.querySelector('.next')
 nextBtn.addEventListener('click', () => updateSlideIndex(slideIndex+1))
-nextBtn.addEventListener('touchstart', () => updateSlideIndex(slideIndex+1))
 const dotBtn = document.querySelectorAll('.dot')
 dotBtn.forEach((dot,i) => {
   dot.addEventListener('click', () => updateSlideIndex(i))
-  dot.addEventListener('touchstart', () => updateSlideIndex(i))
 })
 
 showSlides()
@@ -23,11 +20,11 @@ function updateSlideIndex(i) {
 function showSlides() {
     var slides = document.querySelectorAll(".mySlides");
     slides.forEach(i => {i.style.display = 'none'; dotBtn.forEach(i => i.classList.remove('active'))})
-    if (slideIndex > slides.length - 1) {slideIndex = 0} 
+    if (slideIndex > slides.length - 1) {slideIndex = 0}
     if (slideIndex < 0) {slideIndex = slides.length - 1}
-    slides[slideIndex].style.display = "block"; 
+    slides[slideIndex].style.display = "block";
     dotBtn[slideIndex].classList.add('active')
-    // change every 5 seconds 
-    // ! do not comment out 
+    // change every 5 seconds
+    // ! do not comment out
     intervalId = setTimeout(() => updateSlideIndex(slideIndex+1), 5000)
 }
